f7a7c919a66363c662be239f920a2fd01aee9805
CindyKatoni <katmuema@gmail.com>
2020-10-18 12:35:04 +0300

    created models, super user

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True,null=True)
