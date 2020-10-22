48589cd92e6f5bbbfa6438b050409856bb354db7
CindyKatoni <katmuema@gmail.com>
2020-10-22 21:00:19 +0300

    change db from sqlite to postgres

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'InstaClone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'cindy',
+    'PASSWORD':'cindy123',
     }
 }
 
